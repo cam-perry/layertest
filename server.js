ee840eb9ef0b1794e831d58d9905a2680f762779
Cameron Perry <cameron@homeisroost.com>
2020-04-17 11:47:57 -0400

    Configure a simple node server Docker image

--- server.js
+++ server.js
@@ -4,6 +4,19 @@ const app = express()
 
 app.get('/st', (req, res) => res.send("ok"))
 
+app.get("/", (req, res) => res.send(`
+	<html>
+	<head>
+	<title>Layerci Test Server</title>
+	</head>
+	<body>
+	<h1>Hello</h1>
+	<br/>
+	<p>If this is showing, it worked!</p>
+	</body>
+	</html>
+	`))
+
 app.listen(8000, () => {
    console.log("We're up on :8000")
 })
